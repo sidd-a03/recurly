cafebd9ddcea4eb0f593142cd97f901b8e60642b
Achinta <achintasiddhanta0304@gmail.com>
2026-04-01 19:52:07 +0530

    feat: implement font

--- apps/mobile/app/(tabs)/index.tsx
+++ apps/mobile/app/(tabs)/index.tsx
@@ -8,26 +8,10 @@ const SafeAreaView = styled(RNSafeAreaView)
 export default function Index() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">Index</Text>
-      <Link href="/onboarding" className="mt-4 rounded-lg bg-primary p-4 text-white">Onboarding</Link>
-      <Link href="/(auth)/sign-up" className="mt-4 rounded-lg bg-primary p-4 text-white">Create Account</Link>
-      <Link href="/(auth)/sign-in" className="mt-4 rounded-lg bg-primary p-4 text-white">Sign In</Link>
-      <Link href={{
-        pathname: "/subscriptons/[id]",
-        params: {
-          id: "spotify"
-        }
-      }}>
-        Spotify Subscription
-      </Link>
-      <Link href={{
-        pathname: "/subscriptons/[id]",
-        params: {
-          id: "claude"
-        }
-      }}>
-        Claude Max Subscription
-      </Link>
+      <Text className="text-7xl font-sans-extrabold">Home</Text>
+      <Link href="/onboarding" className="mt-4 font-sans-bold rounded-lg bg-primary p-4 text-white">Onboarding</Link>
+      <Link href="/(auth)/sign-in" className="mt-4 font-sans-bold rounded-lg bg-primary p-4 text-white">Sign In</Link>
+      <Link href="/(auth)/sign-up" className="mt-4 font-sans-bold rounded-lg bg-primary p-4 text-white">Create Account</Link>
     </SafeAreaView>
   );
 }
